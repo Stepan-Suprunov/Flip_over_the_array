9cd5ed3b3e30302b0a4064cc3c2f9de82976f692
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-02 06:17:55 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Перевернуть массив
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,10);
+}
+
+void PrintArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+       Console.Write(collection[i] + " ");
+    }
+Console.WriteLine();
+}
+
+void FlipOver(int[] Collection)
+{
+    int save;
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        save =  array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = save;
+    }
+}
+
+PrintArray(array);
+FlipOver(array);
+PrintArray(array);
